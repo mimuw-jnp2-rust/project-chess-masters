6faa39e6183370cf697481009dce3b53ac19abf9
Agata Zaleska <aga.zaleska.a@gmail.com>
2022-11-23 20:55:48 +0100

    added pieces and greeting function:)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct ChessPiece {
+    piece_type: String,
+}
+
+fn add_pieces(mut commands: Commands) {
+    commands.spawn(ChessPiece {
+        piece_type: String::from("pawn"),
+    });
+    commands.spawn(ChessPiece {
+        piece_type: String::from("king"),
+    });
+    commands.spawn(ChessPiece {
+        piece_type: String::from("bishop"),
+    });
+}
+
+fn greet_pieces(query: Query<&ChessPiece>) {
+    for piece in query.iter() {
+        println!("hello {}!", piece.piece_type);
+    }
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_pieces)
+        .add_system(hello_world)
+        .add_system(greet_pieces)
+        .run();
 }
